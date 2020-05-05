7c03e1bcbd77abe0787554a887789f13b8d82be3
Gerard Banasig <tuxrace@yahoo.com>
2020-05-05 21:52:20 +0800

    Clean up

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,12 +1,5 @@
 import React, {memo} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
