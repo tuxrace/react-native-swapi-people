887352679372e6a716f11fae785d3023bc5b0f3c
User <test@mail.com>
2020-05-05 21:16:32 +0800

    Initial commit

---
+++ src/Main/Main.tsx
@@ -0,0 +1,19 @@
+import React, {memo} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const Main = () => {
+  return (
+    <View>
+      <Text> People Finder </Text>
+    </View>
+  );
+};
+
+export default memo(Main);
